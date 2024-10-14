e40e9d1c79ee56eb2a2afc3907c644cfaca18983
Tri <superglue2294@gmail.com>
2024-10-14 16:58:26 -0400

    push

---
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -0,0 +1,54 @@
+package org.launchcode.codingevents.controllers;
+
+import org.launchcode.codingevents.data.EventData;
+import org.launchcode.codingevents.models.Event;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("events")
+public class EventController {
+
+    @GetMapping
+    public String displayEventPage(Model model) {
+        model.addAttribute("events", EventData.getAll());
+        return "events/index";
+    }
+
+    // lives at /events/create
+    @GetMapping("create")
+    public String renderCreateEventForm() {
+        return "events/create";
+    }
+
+    // lives at /events/create
+    @PostMapping("create")
+    public String processCreateEventForm(@ModelAttribute Event newEvent) {
+        EventData.add(newEvent);
+        return "redirect:/events";
+    }
+
+    @GetMapping("delete")
+    public String displayDeleteEventForm(Model model) {
+        model.addAttribute("title", "Delete Events");
+        model.addAttribute("events", EventData.getAll());
+        return "events/delete";
+    }
+
+    @PostMapping("delete")
+    public String processDeleteEventsForm(@RequestParam(required = false) int[] eventIds) {
+
+        if (eventIds != null) {
+            for (int id : eventIds) {
+                EventData.remove(id);
+            }
+        }
+
+        return "redirect:/events";
+    }
+
+}
